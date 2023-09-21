06c6b540c2bcdbd003367b6a93bb38784f1d13b2
muegan <2216869@sookmyung.ac.kr>
2023-09-21 16:15:11 +0900

    3번

--- main.c
+++ main.c
@@ -4,17 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c, d;
-	int a, b;
+	char c;
+	int a;
 	
 	printf("enter character : ");
 	scanf("%c", &c);
 	
 	a = c - '0';
-	b = a+1;
-	d = b + '0';
 	
-	printf("the next character of %c (%d) is %c (%d)", c,a,d,b); 
+	printf("the next character of %c (%d) is %c (%d)", c, a, c+1,a+1); 
 	
 	
 	
